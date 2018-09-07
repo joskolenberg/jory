9b5c133a8aa486ec43c3674f34e8bf64e0f50085
joskolenberg <joskolenberg@gmail.com>
2018-09-07 20:37:18 +0200

    Added more seed data for tests

---
+++ tests/Models/Instrument.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: joskolenberg
+ * Date: 07-09-18
+ * Time: 20:24
+ */
+
+namespace JosKolenberg\Jory\Tests\Models;
+
+
+class Instrument extends Model
+{
+
+}
