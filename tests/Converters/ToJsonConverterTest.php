473008a7547b88350ff044b9092d61d70f337475
joskolenberg <joskolenberg@gmail.com>
2018-10-20 15:49:18 +0200

    Added offset and limit to converter.

--- tests/Converters/ToJsonConverterTest.php
+++ tests/Converters/ToJsonConverterTest.php
@@ -58,6 +58,8 @@ class ToJsonConverterTest extends TestCase
             ],
             'relations' => [
                 'users' => [
+                    'offset' => 100,
+                    'limit' => 50,
                     'filter' => [
                         'field'    => 'active',
                         'operator' => '=',
@@ -75,7 +77,7 @@ class ToJsonConverterTest extends TestCase
 
         $converter = new ToJsonConverter($jory);
 
-        $this->assertEquals('{"flt":{"and":[{"f":"first_name","v":"Eric"},{"f":"last_name","v":"Clapton"},{"or":[{"f":"band","o":"in","v":["beatles","stones"]},{"and":[{"f":"project","o":"like","v":"Cream"},{"f":"drummer","v":"Ginger Baker"}]}]}]},"srt":{"year":"desc"},"rlt":{"users":{"flt":{"f":"active","o":"=","v":true},"srt":{"name":"asc","id":"desc"}}}}', $converter->get());
+        $this->assertEquals('{"flt":{"and":[{"f":"first_name","v":"Eric"},{"f":"last_name","v":"Clapton"},{"or":[{"f":"band","o":"in","v":["beatles","stones"]},{"and":[{"f":"project","o":"like","v":"Cream"},{"f":"drummer","v":"Ginger Baker"}]}]}]},"srt":{"year":"desc"},"rlt":{"users":{"flt":{"f":"active","o":"=","v":true},"srt":{"name":"asc","id":"desc"},"ofs":100,"lmt":50}}}', $converter->get());
     }
 
     /** @test */
@@ -126,6 +128,8 @@ class ToJsonConverterTest extends TestCase
                         'operator' => '=',
                         'value'    => true,
                     ],
+                    'offset' => 100,
+                    'limit' => 50,
                     'sorts' => [
                         'name' => 'asc',
                         'id' => 'desc',
@@ -138,7 +142,7 @@ class ToJsonConverterTest extends TestCase
 
         $converter = new ToJsonConverter($jory, false);
 
-        $this->assertEquals('{"filter":{"group_and":[{"field":"first_name","value":"Eric"},{"field":"last_name","value":"Clapton"},{"group_or":[{"field":"band","operator":"in","value":["beatles","stones"]},{"group_and":[{"field":"project","operator":"like","value":"Cream"},{"field":"drummer","value":"Ginger Baker"}]}]}]},"sorts":{"year":"desc"},"relations":{"users":{"filter":{"field":"active","operator":"=","value":true},"sorts":{"name":"asc","id":"desc"}}}}', $converter->get());
+        $this->assertEquals('{"filter":{"group_and":[{"field":"first_name","value":"Eric"},{"field":"last_name","value":"Clapton"},{"group_or":[{"field":"band","operator":"in","value":["beatles","stones"]},{"group_and":[{"field":"project","operator":"like","value":"Cream"},{"field":"drummer","value":"Ginger Baker"}]}]}]},"sorts":{"year":"desc"},"relations":{"users":{"filter":{"field":"active","operator":"=","value":true},"sorts":{"name":"asc","id":"desc"},"offset":100,"limit":50}}}', $converter->get());
     }
 
     /** @test */
